1d1d1b82a54da8eb7c6975bc49d87f327a465bfe
Ca7Ac1 <Ca7Ac1@gmail.com>
2021-10-03 15:23:27 -0400

    Finished assignment

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    srand(time(NULL));
+
+    int arr1[10];
+    arr1[0] = 0;
+
+    int i;
+    for (i = 1; i < 10; i++)
+    {
+        arr1[i] = rand();
+    }
+
+    printf("Array:\n");
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d ", arr1[i]);
+    }
+    printf("\n\n");
+
+    int arr2[10];
+    int *ptr1 = arr1;
+    int *ptr2 = arr2;
+
+    for (i = 0; i < 10; i++)
+    {
+        if (i < 5)
+        {
+            ptr2[i] = ptr1[9 - i];
+        }
+        else
+        {
+            *(ptr2 + i) = *(ptr1 + (9 - i));
+        }
+    }
+}
